da27cae16e60d73ddd8fd696756d73a3187db488
maxetek <59445554+maxetek@users.noreply.github.com>
2026-04-11 17:46:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='gym-v6';
+const SHELL=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    if(res&&res.status===200){const c=res.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));}
+    return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
