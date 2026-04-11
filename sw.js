372637287eddc5e47892b7acdbdda5964975548b
maxetek <59445554+maxetek@users.noreply.github.com>
2026-04-11 22:12:32 +0300

    Update service worker cache version and icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gym-v8';
-const SHELL=['./index.html','./manifest.json','./icon-192.jpeg','./icon-512.jpeg'];
+const CACHE='gym-v9';
+const SHELL=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
